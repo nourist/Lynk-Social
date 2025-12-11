acca52fe8c052624f83e09175a645b1b144c1b9a
Nourist <hodinhvys@gmail.com>
2025-12-11 20:50:31 +0700

    fix: fix next build

--- src/app/(root)/chat/page.tsx
+++ src/app/(root)/chat/page.tsx
@@ -1,19 +1,17 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-
 import ChatEmptyState from './_components/chat-empty-state';
 import ChatSidebar from './_components/chat-sidebar';
-import ChatWindow from './_components/chat-window';
+
+// import ChatWindow from './_components/chat-window';
 
 const Chat = () => {
-	const searchParams = useSearchParams();
-	const selectedId = searchParams.get('id');
-
 	return (
 		<div className="flex w-full overflow-hidden">
 			<ChatSidebar />
-			<div className="flex-1 p-6">{!selectedId ? <ChatEmptyState /> : <ChatWindow selectedId={selectedId} />}</div>
+			<div className="flex-1 p-6">
+				<ChatEmptyState />
+			</div>
 		</div>
 	);
 };
